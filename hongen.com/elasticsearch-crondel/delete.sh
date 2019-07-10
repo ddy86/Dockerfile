263f89120552f67e2b0f9075985f5ddb54063fd0
daidongyang <henuddy@gmail.com>
2019-07-10 15:47:42 +0800

    elastic log delete v1.0

---
+++ hongen.com/elasticsearch-crondel/delete.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+day_15_ago=$(date -d "-15 days" "+%Y.%m.%d")
+day_30_ago=$(date -d "-30 days" "+%Y-%m-%d")
+
+echo "`date` deleting logstash $day_15_ago"
+curl -XDELETE "http://elasticsearch.default.svc.hongen.com:9200/logstash-$day_15_ago"
+
+echo "`date` deleting device and weixin log $day_30_ago"
+curl -XDELETE "http://elasticsearch.default.svc.hongen.com:9200/logs_*-$day_30_ago"
+
