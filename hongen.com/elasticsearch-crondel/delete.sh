8f5a7c9c75516562e2c23cc78e936ca29c11da76
daidongyang <henuddy@gmail.com>
2020-05-26 14:25:14 +0800

    change elastic index id --> logs_*

--- hongen.com/elasticsearch-crondel/delete.sh
+++ hongen.com/elasticsearch-crondel/delete.sh
@@ -3,7 +3,7 @@ day_15_ago=$(date -d "-15 days" "+%Y.%m.%d")
 day_30_ago=$(date -d "-30 days" "+%Y-%m-%d")
 
 echo "`date` deleting logstash $day_15_ago"
-curl -XDELETE "http://elasticsearch.default.svc.hongen.com:9200/logstash*-$day_15_ago"
+curl -XDELETE "http://elasticsearch.default.svc.hongen.com:9200/logs_*-$day_15_ago"
 echo -e "\n\n"
 echo "`date` deleting device and weixin log $day_30_ago"
 curl -XDELETE "http://elasticsearch.default.svc.hongen.com:9200/logs_*-$day_30_ago"
