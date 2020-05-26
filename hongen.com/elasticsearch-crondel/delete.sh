8e73386fde56bf01febda994f119b12103a87357
daidongyang <henuddy@gmail.com>
2020-05-26 14:36:34 +0800

    use cluster.local domain

--- hongen.com/elasticsearch-crondel/delete.sh
+++ hongen.com/elasticsearch-crondel/delete.sh
@@ -3,8 +3,8 @@ day_15_ago=$(date -d "-15 days" "+%Y.%m.%d")
 day_30_ago=$(date -d "-30 days" "+%Y-%m-%d")
 
 echo "`date` deleting logstash $day_15_ago"
-curl -XDELETE "http://elasticsearch.default.svc.hongen.com:9200/logs_*-$day_15_ago"
+curl -XDELETE "http://elasticsearch.default.svc.cluster.local:9200/logs_*-$day_15_ago"
 echo -e "\n\n"
 echo "`date` deleting device and weixin log $day_30_ago"
-curl -XDELETE "http://elasticsearch.default.svc.hongen.com:9200/logs_*-$day_30_ago"
+curl -XDELETE "http://elasticsearch.default.svc.cluster.local:9200/logs_*-$day_30_ago"
 
