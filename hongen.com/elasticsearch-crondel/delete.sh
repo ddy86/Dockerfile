364c90f8cbc718542e5457b38e540e0f6c77b342
daidongyang <henuddy@gmail.com>
2020-05-28 14:46:23 +0800

    add ENV params

--- hongen.com/elasticsearch-crondel/delete.sh
+++ hongen.com/elasticsearch-crondel/delete.sh
@@ -10,12 +10,13 @@ if [ -z "${DAYS_RESERVE_EKLOGS}" ]; then
 fi
 
 
-day_weixin_ago=$(date -d "-$DAYS_RESERVE_WEIXINLOGS days" "+%Y.%m.%d")
-day_ek_ago=$(date -d "-$DAYS_RESERVE_EKLOGS days" "+%Y-%m-%d")
-
+day_ek_ago=$(date -d "-$DAYS_RESERVE_EKLOGS days" "+%Y.%m.%d")
 echo "`date` deleting logstash $day_ek_ago"
 curl -XDELETE "http://elasticsearch.default.svc.cluster.local:9200/logs_*-$day_ek_ago"
+
 echo -e "\n\n"
+
+day_weixin_ago=$(date -d "-$DAYS_RESERVE_WEIXINLOGS days" "+%Y-%m-%d")
 echo "`date` deleting device and weixin log $day_weixin_ago"
 curl -XDELETE "http://elasticsearch.default.svc.cluster.local:9200/logs_*-$day_weixin_ago"
 
